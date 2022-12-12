6004b79230aecd6c7114e51b2738796660516add
ChrisCollins800 <Christopher.Collins2@students.ittralee.ie>
2022-12-12 01:04:18 -0300

    Asteroid Collision & Losing

--- RAD/Assets/asteroidMovement.cs
+++ RAD/Assets/asteroidMovement.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class asteroidMovement : MonoBehaviour
 {
     public float speed = 3;
+    Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(-speed, 0, 0)*Time.deltaTime;
+
+        if (transform.position.x <= -34)
+        {
+            transform.position = startPos;
+            speed += 2;
+        }
+
     }
 }

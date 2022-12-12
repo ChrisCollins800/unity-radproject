b1245223f915caf97c717249f9dd1a2765871b24
ChrisCollins800 <Christopher.Collins2@students.ittralee.ie>
2022-12-11 22:48:08 -0300

    Player Movement

--- RAD/Assets/playerMovement.cs
+++ RAD/Assets/playerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cubecontrol : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
     public float speed = 2;
 

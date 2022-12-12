88691fb5210df08def7e124b46e10ebfcdb2a5a5
ChrisCollins800 <Christopher.Collins2@students.ittralee.ie>
2022-12-11 23:24:03 -0300

    Asteroids Movement

---
+++ RAD/Assets/asteroidMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroidMovement : MonoBehaviour
+{
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-speed, 0, 0)*Time.deltaTime;
+    }
+}

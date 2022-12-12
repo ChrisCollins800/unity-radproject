26c4310147dbdc8cd1fdcf663ede8008ebc52ede
ChrisCollins800 <Christopher.Collins2@students.ittralee.ie>
2022-12-12 11:31:34 -0300

    Polishing Restart Screen

--- RAD/Assets/RestartScript.cs
+++ RAD/Assets/RestartScript.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class RestartScript : MonoBehaviour
 {
+    public GameObject canvas;
     public void RestartButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        canvas.SetActive(false);
     }
 }
